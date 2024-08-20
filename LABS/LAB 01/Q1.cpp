3b14ac9330d1c279862d108de6e4c581f12f8eb1
iamrayyan1 <142688529+iamrayyan1@users.noreply.github.com>
2024-08-20 22:55:40 +0500

    Update Q1.cpp

--- LABS/LAB 01/Q1.cpp
+++ LABS/LAB 01/Q1.cpp
@@ -1,1 +1,50 @@
+#include<iostream>
+using namespace std;
 
+class Account{
+    private:
+        int *balance;
+    public:
+
+        Account(){
+            balance = new int(0);
+        }
+        Account(int bal){
+            balance = new int(bal);
+        }
+        Account(const Account &other){
+            balance = new int(*other.balance);
+        }
+
+        Account &operator=(const Account& other){
+            if(this != &other){
+                delete balance;
+                balance = new int(*other.balance);
+            }
+            return *this;
+        }
+
+        int get_balance(){
+            return *balance;
+        }
+        void deduct_balance(int amount){
+            *balance -= amount;
+        }
+
+        ~Account(){
+            delete balance;
+        }
+};
+
+int main(){
+    Account acc1;
+    Account acc2(1000);
+    Account acc3(acc2);
+
+    cout << "Account 1 Balance: " << acc1.get_balance() << endl;
+    cout << "Account 2 Balance: " << acc2.get_balance() << endl;
+
+    acc3.deduct_balance(200);
+    cout << "Account 3 Balance: " << acc3.get_balance() << endl;
+    cout << "Account 2 Balance: " << acc2.get_balance() << endl;
+}
