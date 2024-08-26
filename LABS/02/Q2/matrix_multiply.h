9b32f06e2155789dbef15e1164c66bac63633c03
iamrayyan1 <142688529+iamrayyan1@users.noreply.github.com>
2024-08-26 05:50:21 +0000

    ...

--- LABS/02/Q2/matrix_multiply.h
+++ LABS/02/Q2/matrix_multiply.h
@@ -1,6 +1,6 @@
 #ifndef MATRIX_MULTIPLY_H
 #define MATRIX_MULTIPLY_H
 
-int matrixMultiplication(int A[][100], int B[][100], int row1, int col1, int row2, int col2);
+void matrixMultiply(const int** A, const int** B, int** C, int m, int n, int p);
 
 #endif 
